e4b1c423233a2cc986f85bfd381dc57edfc5b1e4
fangxiang <fangxiang@immotors.com>
2023-06-05 19:18:53 +0800

    feat: add

--- chapter1-1/src/main/java/com/didispace/chapter11/Chapter11Application.java
+++ chapter1-1/src/main/java/com/didispace/chapter11/Chapter11Application.java
@@ -3,11 +3,14 @@ package com.didispace.chapter11;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author fangxiang
+ */
 @SpringBootApplication
 public class Chapter11Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Chapter11Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Chapter11Application.class, args);
+  }
 
 }
--- chapter1-1/src/test/java/com/didispace/chapter11/Chapter11ApplicationTests.java
+++ chapter1-1/src/test/java/com/didispace/chapter11/Chapter11ApplicationTests.java
@@ -10,25 +10,28 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+;
 
 @SpringBootTest
 public class Chapter11ApplicationTests {
-	private MockMvc mvc;
 
-//	使用MockServletContext来构建一个空的WebApplicationContext，
+  private MockMvc mvc;
+
+  //	使用MockServletContext来构建一个空的WebApplicationContext，
 //	这样我们创建的HelloController就可以在@Before函数中创建并传递到MockMvcBuilders.standaloneSetup()函数中。
-	@Before
-	public void setUp() {
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-	}
+  @Before
+  public void setUp() {
+    mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+  }
 
-	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World")));
-	}
+  @Test
+  public void getHello() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(equalTo("Hello World")));
+  }
 }
 
 
